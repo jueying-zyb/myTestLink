c9aa0f7b4af29712a04c674f29a9bdc439bb9ddb
joy.zhou <_31huiyi>
2019-05-21 10:02:43 +0800

    first commit

---
+++ src/com/zyb/test/MyTest.java
@@ -0,0 +1,22 @@
+package com.zyb.test;
+
+import org.junit.Test;
+
+public class MyTest {
+	
+	@Test
+	public void test001() {
+		System.out.println("test001");
+	}
+	
+	@Test
+	public void test002() {
+		System.out.println("test002");
+	}
+	
+	@Test
+	public void test003() {
+		System.out.println("test003");
+	}
+
+}
